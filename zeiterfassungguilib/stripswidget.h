cb5f54aa916dc9c6f20e979f9b8b82320ff5ef75
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2018-05-25 20:27:28 +0200

    Added display of soll ist time to StripsWidget

--- zeiterfassungguilib/stripswidget.h
+++ zeiterfassungguilib/stripswidget.h
@@ -6,6 +6,7 @@
 #include <QTime>
 
 #include "zeiterfassungguilib_global.h"
+#include "replies/getdayinforeply.h"
 #include "replies/getbookingsreply.h"
 #include "replies/gettimeassignmentsreply.h"
 
@@ -27,7 +28,8 @@ public:
     QBoxLayout *headerLayout() const;
     QBoxLayout *stripsLayout() const;
 
-    QLabel *label() const;
+    QLabel *label0() const;
+    QLabel *label1() const;
 
     const QDate &date() const;
     void setDate(const QDate &date);
@@ -35,6 +37,7 @@ public:
     bool highlighted() const;
     void setHighlighted(bool highlighted);
 
+    const GetDayinfoReply::Dayinfo &dayinfo() const;
     const QVector<GetBookingsReply::Booking> &bookings() const;
     const QVector<GetTimeAssignmentsReply::TimeAssignment> &timeAssignments() const;
 
@@ -42,12 +45,14 @@ public:
     const QTime &lastTimeAssignmentStart() const;
     const QTime &minimumTime() const;
     bool refreshing() const;
+    bool refreshingDayinfo() const;
     bool refreshingBookings() const;
     bool refreshingTimeAssignments() const;
     bool startEnabled() const;
     bool endEnabled() const;
 
     void refresh();
+    void refreshDayinfo();
     void refreshBookings(bool createLabel = true);
     void refreshTimeAssignments(bool createLabel = true);
     bool createStrips();
@@ -57,6 +62,7 @@ Q_SIGNALS:
     void dateChanged(const QDate &date);
     void highlightedChanged(bool highlighted);
 
+    void dayinfoChanged(const GetDayinfoReply::Dayinfo &dayinfo);
     void bookingsChanged(const QVector<GetBookingsReply::Booking> &bookings);
     void timeAssignmentsChanged(const QVector<GetTimeAssignmentsReply::TimeAssignment> &timeAssignments);
 
@@ -64,12 +70,14 @@ Q_SIGNALS:
     void lastTimeAssignmentStartChanged(const QTime &lastTimeAssignmentStart);
     void minimumTimeChanged(const QTime &minimumTime);
     void refreshingChanged(bool refreshing);
+    void refreshingDayinfoChanged(bool refreshingDayinfo);
     void refreshingBookingsChanged(bool refreshingBookings);
     void refreshingTimeAssignmentsChanged(bool refreshingTimeAssignments);
     void startEnabledChanged(bool startEnabled);
     void endEnabledChanged(bool endEnabled);
 
 private Q_SLOTS:
+    void getDayinfoFinished();
     void getBookingsFinished();
     void getTimeAssignmentsFinished();
 
@@ -87,11 +95,12 @@ private:
     QBoxLayout *m_headerLayout;
     QBoxLayout *m_stripsLayout;
 
-    QLabel *m_label;
+    QLabel *m_label[2];
 
     QDate m_date;
     bool m_highlighted;
 
+    GetDayinfoReply::Dayinfo m_dayinfo;
     QVector<GetBookingsReply::Booking> m_bookings;
     QVector<GetTimeAssignmentsReply::TimeAssignment> m_timeAssignments;
 
@@ -99,11 +108,15 @@ private:
     QTime m_lastTimeAssignmentStart;
     QTime m_minimumTime;
     bool m_refreshing;
+    bool m_refreshingDayinfo;
     bool m_refreshingBookings;
     bool m_refreshingTimeAssignments;
     bool m_startEnabled;
     bool m_endEnabled;
 
+    std::unique_ptr<GetDayinfoReply> m_getDayinfoReply;
     std::unique_ptr<GetBookingsReply> m_getBookingsReply;
     std::unique_ptr<GetTimeAssignmentsReply> m_getTimeAssignmentsReply;
+
+    static const QStringList m_weekDays;
 };
